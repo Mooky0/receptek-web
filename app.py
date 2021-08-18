8bd68d11e8f3dac4c97d73b36fd4230f85bf9524
Mooky0 <tothgabor2003@gmail.com>
2021-08-18 04:55:48 +0200

    az oldalsávban működik felsorolás, de még linkekként van ott a db-ből szedi ki az infót és sorolja fel a sidenav magssága jó, nem kell mókolni, pont méret, minden abalk méreten

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 
+import dbhandler
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+   tomb = dbhandler.init_lista()
+   lentomb=len(tomb)
+   return render_template('index.html', tomb=tomb, lentomb=lentomb)
 
 if __name__ == '__main__':
    app.run()
+
