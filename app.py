4224c7b16fef3e644d7c3d6919db499893011677
Mooky0 <tothgabor2003@gmail.com>
2021-08-26 18:58:30 +0200

    kereső kezdősik lenni
    A mezőbe beírt értéket beveszi, és kiírja egy oldalra, ezt lehet továbbfejleszteni majd.

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 import dbhandler
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index/')
 def index():
    tomb = dbhandler.init_lista()
    lentomb=len(tomb)
    return render_template('index.html', tomb=tomb, lentomb=lentomb)
+
+@app.route('/kereso/', methods=['POST', 'GET'])
+def index_post():
+   if request.method == "POST":
+      keres = request.form['kereso']
+      print(keres)
+      return redirect(url_for('kereses', keres = keres))
+   else:
+      pass
+
+@app.route('/kereses/<keres>')
+def kereses(keres):
+   return 'A keresett érték: %s' % keres
 
 if __name__ == '__main__':
    app.run()
